a1f15a5d7750dd536696160ea0da6c7b6028bbce
Bruno-mrBLONDE <123082731+Leemonztuff@users.noreply.github.com>
2025-09-08 11:41:28 -0300

    Update productos.js

--- src/data/productos.js
+++ src/data/productos.js
@@ -1,18 +1,15 @@
-export default [
-  { name:"Pomada OS 100g", price:10713.06, bonus:"6x1/8x2" },
-  { name:"Pocket OS 50g", price:8073.18, bonus:"6x1/8x2" },
-  { name:"Pomada Mate 50g", price:10219.57, bonus:"6x1/8x2" },
-  { name:"Stardust Polvo Modelador 10cc", price:10053.80, bonus:"6x1/8x2" },
-  { name:"Pomada Líquida x 120ml", price:8302.61, bonus:"6x1/8x2" },
-  { name:"Shampoo Fortalecedor 2en1 200g", price:10310.58, bonus:"6x1/8x2" },
-  { name:"Aceite para Barba 20ml N°1 Azul", price:9956.17, bonus:"6x1/8x2" },
-  { name:"Aceite para Barba 20ml N°2 Rojo", price:9956.17, bonus:"6x1/8x2" },
-  { name:"Aceite para Barba 20ml N°3 Amarillo", price:9956.17, bonus:"6x1/8x2" },
-  { name:"Bálsamo Barba con Romero 30g", price:8162.96, bonus:"6x1/8x2" },
-  { name:"After Shave 100g", price:8480.46, bonus:"6x1/8x2" },
-  { name:"Válvula Recargable 250g para Gel Mr B", price:1500.25, bonus:"6x1/8x2" },
-  { name:"Gel Afeitar Profesional 1080ml", price:14940.09, bonus:"6x1/8x2" },
-  { name:"Crema de afeitar x 400g", price:15735.42, bonus:"6x1/8x2" },
-  { name:"Capas", price:14026 },
-  { name:"Cajones Exhibidores Mr. Blonde", price:34145 }
+const productos = [
+  {
+    name: "Pomada OS 100g",
+    price: 2650,
+    img: "https://acdn.mitiendanube.com/stores/001/189/845/products/pomada-os-mr-blonde1-177e9c2e4c5c31aeb816979608245105-1024-1024.webp"
+  },
+  {
+    name: "Stardust Polvo Modelador 10cc",
+    price: 3995.7,
+    img: "https://acdn.mitiendanube.com/stores/001/189/845/products/stardust1-e328c9dcf864d1c6d516980804999476-1024-1024.webp"
+  },
+  // agrega más productos aquí...
 ];
+
+export default productos;
