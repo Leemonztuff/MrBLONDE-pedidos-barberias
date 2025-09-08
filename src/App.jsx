34cb5543a73a7a9818516fa581c23110ec38afd7
Bruno-mrBLONDE <123082731+Leemonztuff@users.noreply.github.com>
2025-09-08 12:57:20 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
-import React, { useState } from "react";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Pedido from "./components/Pedido";
 import Bienvenida from "./components/Bienvenida";
-import "./index.css";
+import AdminPanel from "./pages/AdminPanel";
 
 export default function App() {
-  const [iniciar, setIniciar] = useState(false);
-
   return (
-    <div>
-      {!iniciar ? (
-        <Bienvenida onComenzar={() => setIniciar(true)} />
-      ) : (
-        <Pedido onVolver={() => setIniciar(false)} />
-      )}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={
+          <React.Fragment>
+            <Bienvenida />
+          </React.Fragment>
+        } />
+        <Route path="/pedido" element={<Pedido />} />
+        <Route path="/admin" element={<AdminPanel />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
